6061d2daec0a5803aca3c436d50602ea6ee1bb5d
Philip J. Grandinetti <43141014+pjgrandinetti@users.noreply.github.com>
2025-05-11 17:52:48 -0400

    no static at all

--- docs/conf.py
+++ docs/conf.py
@@ -44,4 +44,4 @@ warnings.filterwarnings(
 
 # -- Options for HTML output -------------------------------------------------
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_static_path = []
